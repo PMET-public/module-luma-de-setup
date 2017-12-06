f7a4bfbe794544d86384a032c9586d8ab104a5b2
Jeffrey Britts <jbritts@magento.com>
2017-12-06 09:43:32 -0500

    Check for existing store and update if exists

--- Setup/InstallData.php
+++ Setup/InstallData.php
@@ -10,17 +10,47 @@ use Magento\Framework\Setup;
 
 class InstallData implements Setup\InstallDataInterface
 {
+    /**
+     * @var \Magento\Store\Model\StoreFactory
+     */
     private $storeView;
 
+    /**
+     * @var \Magento\Store\Api\StoreRepositoryInterfaceFactory
+     */
+    private $storeRepositoryFactory;
+
+    /**
+     * @var \Magento\Store\Model\WebsiteRepository
+     */
     private $websiteRepository;
 
+    /**
+     * @var \Magento\Store\Model\Website
+     */
     private $website;
 
+    /**
+     * @var \Magento\Framework\App\State
+     */
     private $state;
 
+    /**
+     * @var \Magento\Config\Model\ResourceModel\Config
+     */
     private $resourceConfig;
 
+    /**
+     * InstallData constructor.
+     * @param \Magento\Store\Model\StoreFactory $storeView
+     * @param \Magento\Store\Api\StoreRepositoryInterfaceFactory $storeRepositoryFactory
+     * @param \Magento\Store\Model\WebsiteRepository $websiteRepository
+     * @param \Magento\Store\Model\Website $website
+     * @param \Magento\Framework\App\State $state
+     * @param \Magento\Config\Model\ResourceModel\Config $resourceConfig
+     */
     public function __construct(\Magento\Store\Model\StoreFactory $storeView,
+                                \Magento\Store\Api\StoreRepositoryInterfaceFactory $storeRepositoryFactory,
                                 \Magento\Store\Model\WebsiteRepository $websiteRepository,
                                 \Magento\Store\Model\Website $website,
                                 \Magento\Framework\App\State $state,
@@ -28,6 +58,7 @@ class InstallData implements Setup\InstallDataInterface
     )
     {
         $this->storeView = $storeView;
+        $this->storeRepositoryFactory = $storeRepositoryFactory;
         $this->websiteRepository = $websiteRepository;
         $this->website = $website;
         $this->config = require 'Config.php';
@@ -41,10 +72,13 @@ class InstallData implements Setup\InstallDataInterface
     }
 
 
-
-
+    /**
+     * @param Setup\ModuleDataSetupInterface $setup
+     * @param Setup\ModuleContextInterface $moduleContext
+     */
     public function install(Setup\ModuleDataSetupInterface $setup, Setup\ModuleContextInterface $moduleContext)
     {
+
         //get id of website by name
         $_websiteId = $this->websiteRepository->get($this->config['website'])->getId();
 
@@ -60,7 +94,13 @@ class InstallData implements Setup\InstallDataInterface
         }
 
         //add new store
+
         $newStore = $this->storeView->create();
+        //check if view exists, if it does load and update
+        $existingStoreId = $this->getExistingStoreId($this->config['newViewCode']);
+        if($existingStoreId !=0){
+            $newStore->load($existingStoreId);
+        }
         $newStore->setName($this->config['newViewName']);
         $newStore->setCode($this->config['newViewCode']);
         $newStore->setWebsiteId($_websiteId);
@@ -76,5 +116,21 @@ class InstallData implements Setup\InstallDataInterface
         $defaultStore->save();
 
         $this->resourceConfig->saveConfig("general/locale/code", "de_DE", "stores", $newStore->getId());
+    }
+
+    /**
+     * @param $storeCode string
+     * @return int
+     */
+    public function getExistingStoreId($storeCode){
+        $storeRepository = $this->storeRepositoryFactory->create();
+        $stores=$storeRepository->getList();
+        foreach($stores as $store){
+            if($store->getCode()==$storeCode){
+                return $store->getId();
+                break;
+            }
+        }
+        return 0;
     }
 }
